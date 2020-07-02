fe73c395d03e73ab87fd1a904f7dd3e34b0f1887
Bazzan <sebake01@gmail.com>
2020-07-02 12:02:48 +0100

    init

---
+++ ProcedualAnimationSpider/Assets/Scripts/SpiderController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+
+
+    [SerializeField] private Transform headBone;
+    [SerializeField] private float headTurningSpeed;
+    [SerializeField] private float headClampAngel;
+
+
+
+    private void LateUpdate()
+    {
+        Quaternion currentLocalRotation = headBone.localRotation;
+
+        headBone.localRotation = Quaternion.identity;
+
+        Vector3 worldTargetLookDir = target.position - headBone.position;
+        Vector3 localTargetLookDir = headBone.parent.InverseTransformDirection(worldTargetLookDir);
+
+
+        localTargetLookDir = Vector3.RotateTowards(
+            Vector3.forward,
+            localTargetLookDir,
+            Mathf.Deg2Rad * headClampAngel,
+            0);
+
+        Quaternion localTargetRotation = Quaternion.LookRotation(localTargetLookDir, transform.up);
+
+        headBone.rotation = Quaternion.Slerp(currentLocalRotation, 
+            localTargetRotation,
+            1 - Mathf.Exp(-headTurningSpeed * Time.deltaTime));
+
+
+    }
+
+
+}
