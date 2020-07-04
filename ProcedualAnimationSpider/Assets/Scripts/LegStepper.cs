7b51df80fe7ec0f79531415870dc7d4e24a8a92d
Bazzan <sebake01@gmail.com>
2020-07-04 18:14:10 +0200

    Animation And controller implemented

---
+++ ProcedualAnimationSpider/Assets/Scripts/LegStepper.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegStepper : MonoBehaviour
+{
+    [SerializeField] public Transform HomeTransform;
+    [SerializeField] private float wantToStepAtDistance;
+    [SerializeField] private float moveDuration;
+
+    [SerializeField] private float stepOverShootFraction;
+
+    public bool isMoving;
+
+
+
+
+    public void TryToMove()
+    {
+        if (isMoving) return;
+
+        float distanceFromHome = Vector3.Distance(transform.position, HomeTransform.position);
+
+        if(distanceFromHome > wantToStepAtDistance)
+        {
+            StartCoroutine(MoveToHome());
+        }
+
+    }
+
+    IEnumerator MoveToHome()
+    {
+
+        isMoving = true;
+
+        Quaternion startRot = transform.rotation;
+        Vector3 startPoint = transform.position;
+
+
+        Quaternion endRot = HomeTransform.rotation;
+
+        Vector3 homeDir = HomeTransform.position - transform.position;
+
+        float overShootDistance = wantToStepAtDistance * stepOverShootFraction;
+
+        Vector3 overShootVector = homeDir * overShootDistance;
+
+        overShootVector = Vector3.ProjectOnPlane(overShootVector, Vector3.up);
+
+
+        Vector3 endPoint = HomeTransform.position + overShootVector;
+
+        Vector3 centerPoint = (startPoint + endPoint) / 2;
+
+        centerPoint += HomeTransform.up * Vector3.Distance(startPoint, endPoint) / 2;
+
+        float timePassed = 0;
+
+        while(timePassed < moveDuration)
+        {
+            timePassed += Time.deltaTime;
+
+            float normalizedTime = timePassed / moveDuration;
+
+            normalizedTime = Easing.Cubic.InOut(normalizedTime);
+
+            transform.position = Vector3.Lerp(
+                Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                normalizedTime);
+
+
+
+            transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+
+            yield return null;
+        }
+
+        isMoving = false;
+
+
+    }
+
+}
--- ProcedualAnimationSpider/Assets/Scripts/SpiderController.cs
+++ ProcedualAnimationSpider/Assets/Scripts/SpiderController.cs
@@ -8,12 +8,83 @@ public class SpiderController : MonoBehaviour
     [SerializeField] private Transform target;
 
 
-
+    //head
     [SerializeField] private Transform headBone;
     [SerializeField] private float headTurningSpeed;
     [SerializeField] private float headClampAngel;
 
+    //Legs
+    [SerializeField] LegStepper frontLeftLegStepper;
+    [SerializeField] LegStepper midLeftLegStepper;
+    [SerializeField] LegStepper backLeftLegStepper;
+    [SerializeField] LegStepper frontRightLegStepper;
+    [SerializeField] LegStepper midRightLegStepper;
+    [SerializeField] LegStepper backRightLegStepper;
 
+
+    Rigidbody rigidbody;
+    Vector3 forwardSpeed = new Vector3(0, 0, 1);
+    float maxSpeed = 4;
+    Quaternion startRotation = Quaternion.identity;
+
+    Vector3 dir;
+
+    private void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+
+    private void Update()
+    {
+        //transform.rotation.z = startRotation.z;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += this.transform.forward * 10 * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * -100 * Time.deltaTime);
+        }else if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+        }
+        
+
+    }
+
+
+    IEnumerator LegUpdateCoroutine()
+    {
+
+        while (true)
+        {
+            do
+            {
+                frontLeftLegStepper.TryToMove();
+                backLeftLegStepper.TryToMove();
+
+                frontRightLegStepper.TryToMove();
+                backRightLegStepper.TryToMove();
+                yield return null;
+
+            } while (backLeftLegStepper.isMoving || frontLeftLegStepper.isMoving || backRightLegStepper.isMoving || frontRightLegStepper.isMoving);
+            do
+            {
+                midLeftLegStepper.TryToMove();
+                midRightLegStepper.TryToMove();
+                yield return null;
+            } while (midLeftLegStepper.isMoving || midRightLegStepper.isMoving);
+        }
+    }
+    
 
     private void LateUpdate()
     {
